3d9ada52f5b315b1ba5ce35bac5b192da033eefb
xmmmmmovo <13256172360@163.com>
2020-03-31 08:30:50 +0800

    change

---
+++ DS/include/ds/string.hpp
@@ -0,0 +1,13 @@
+//
+// Created by xmmmmmovo on 2020/3/30.
+//
+
+#ifndef DS_STRING_HPP
+#define DS_STRING_HPP
+
+namespace ds{
+class string {
+};
+}
+
+#endif // DS_STRING_HPP
---
+++ DS/include/ds/vector.hpp
@@ -0,0 +1,123 @@
+//
+// Created by xmmmmmovo on 2020/3/26.
+//
+
+#ifndef DS_VECTOR_HPP
+#define DS_VECTOR_HPP
+
+#include <exception>
+#include <iostream>
+#include <string>
+
+using std::istream;
+using std::ostream;
+using std::string;
+
+namespace ds {
+template <typename T>
+class vector {
+  public:
+    vector();
+
+    vector(size_t size);
+
+    vector(size_t size, T value);
+
+    size_t size();
+
+    void append(T x);
+
+    T &operator[](size_t index) {
+        int i = (int)index;
+        if (i > _size || i < -_size) {
+            throw std::out_of_range("数组越界 out of range");
+        }
+
+        if (i < 0) {
+            return elem[ _size + i ];
+        }
+
+        return elem[ i ];
+    }
+
+    T &operator[](string s) {}
+
+    friend ostream &operator<<(ostream &o, vector<T> &v) {
+        int j = 0;
+        o << "[";
+        for (; j < v._size - 1; ++j) {
+            o << v.elem[ j ] << ", ";
+        }
+        o << v.elem[ j ] << "]";
+        return o;
+    }
+
+  private:
+    T *    elem;
+    size_t _size;
+    size_t it;
+
+    void _append(T &t);
+
+    void _alloc();
+};
+
+template <typename T>
+inline size_t vector<T>::size() {
+    return _size;
+}
+
+template <typename T>
+vector<T>::vector() {
+    it    = 16; // 对齐
+    elem  = new T[ it ];
+    _size = 0;
+}
+
+template <typename T>
+vector<T>::vector(size_t size) {
+    it    = size;
+    elem  = new T[ size ];
+    _size = 0;
+}
+
+template <typename T>
+vector<T>::vector(size_t size, T value) {
+    it   = size;
+    elem = new T[ size ];
+    for (int i = 0; i < size; ++i) {
+        elem[ i ] = value;
+    }
+    _size = size;
+}
+
+template <typename T>
+void vector<T>::append(T x) {
+    _append(x);
+}
+
+template <typename T>
+void vector<T>::_append(T &t) {
+    if (++_size > it) {
+        _alloc();
+    }
+    elem[ _size - 1 ] = t;
+}
+
+template <typename T>
+void vector<T>::_alloc() {
+    T *tmp = elem;
+    try {
+        elem = new T[ it <<= 2u ];
+    } catch (std::bad_alloc &e) {
+        throw e;
+    }
+    for (int i = 0; i < _size - 1; ++i) {
+        elem[ i ] = tmp[ i ];
+    }
+    delete[] tmp;
+}
+
+} // namespace ds
+
+#endif // DS_VECTOR_HPP
