c767727abd97b0f4e8a197015904a1610e488ca5
xmmmmmovo <13256172360@163.com>
2020-05-10 15:03:18 +0800

    vector

--- DS/include/ds/vector.hpp
+++ DS/include/ds/vector.hpp
@@ -7,14 +7,19 @@
 
 #include <algorithm>
 #include <iostream>
+#include <memory>
 
 namespace ds {
 
+using std::align_val_t;
 using std::copy_n;
+using std::destroy;
 using std::exchange;
 using std::max;
 using std::move;
 using std::size_t;
+using std::uninitialized_default_construct;
+using std::uninitialized_move;
 
 template<typename T>
 class vector {
@@ -24,14 +29,17 @@ private:
     T *end_   = nullptr;
 
     void alloc_and_move(const size_t new_capacity) {
-        T *new_first = new T[ new_capacity ];
-        last_        = move(first_, last_, new_first);
-        delete[] first_;
+        T *new_first = alloc_raw(new_capacity);
+        T *new_last  = uninitialized_move(first_, last_, new_first);
+        destroy(first_, last_);
+        free_raw(first_);
         first_ = new_first;
+        last_  = new_last;
         end_   = first_ + new_capacity;
     }
 
     size_t calcu_growth(const size_t new_size) const {
+        //        const size_t geometric = capacity() << 2;
         const size_t geometric = capacity() + capacity() / 2;
         return max(geometric, new_size);
     }
@@ -43,17 +51,33 @@ private:
         alloc_and_move(calcu_growth(new_size));
     }
 
+    static T *alloc_raw(const size_t size) {
+        void *data = operator new(size * sizeof(T), align_val_t(alignof(T)));
+        return static_cast<T *>(data);
+    }
+
+    static void free_raw(void *data) {
+        operator delete(data, align_val_t(alignof(T)));
+    }
+
 public:
     vector() = default;
 
-    explicit vector(const size_t size)
-        : first_(new T[ size ]()), last_(first_ + size) {}
+    explicit vector(const size_t size) {
+        first_ = alloc_raw(size);
+        end_ = last_ = first_ + size;
+        uninitialized_default_construct(first_, last_);
+    }
 
-    ~vector() noexcept { delete[] first_; }
+    ~vector() noexcept {
+        destroy(first_, last_);
+        free_raw(first_);
+    }
 
     vector(const vector &other) {
-        first_ = new T[ other.size() ];
-        end_ = last_ = std::copy(other.first_, other.last_, first_);// 深拷贝
+        first_ = alloc_raw(other.size());
+        end_ = last_ = first_ + other.size();
+        uninitialized_default_construct(other.first_, other.last_, first_);
     }
 
     vector &operator=(const vector &other) {
@@ -61,8 +85,9 @@ public:
             return *this;
         }
 
-        delete[] first_;
-        first_ = new T[ other.size() ];
+        destroy(first_, last_);
+        free_raw(first_);
+        first_ = alloc_raw(other.size());
         end_ = last_ = std::copy(other.first_, other.last_, first_);
         return *this;
     }
--- DS/test/vector_test.cpp
+++ DS/test/vector_test.cpp
@@ -4,7 +4,35 @@
 #include "../include/ds/vector.hpp"
 #include <iostream>
 
+using ds::vector;
+
+struct LifetimeLogger {
+    LifetimeLogger() { std::puts("default c'tor"); }
+    ~LifetimeLogger() noexcept { std::puts("d'tor"); }
+    LifetimeLogger(const LifetimeLogger &) { std::puts("copy c'tor"); }
+    LifetimeLogger(LifetimeLogger &&) noexcept { std::puts("move c'tor"); }
+    LifetimeLogger &operator=(const LifetimeLogger &) {
+        std::puts("copy operator=");
+        return *this;
+    }
+    LifetimeLogger &operator=(LifetimeLogger &&) noexcept {
+        std::puts("move operator=");
+        return *this;
+    }
+};
+
 int main(int argc, char *argv[]) {
+    vector<int> v;
+
+    for (int i = 0; i < 2000; ++i) {
+        v.emplace_back(i);
+        std::cout << "cap is : " << v.capacity() << std::endl;
+    }
+
+    vector<LifetimeLogger> vl;
+
+    vl.emplace_back(LifetimeLogger());
+    vl.emplace_back(LifetimeLogger());
 
     return 0;
 }
