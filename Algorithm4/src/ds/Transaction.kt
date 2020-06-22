c86c4c686c8c9d3def8907462dbbd79e5d29010d
xmmmmmovo <13256172360@163.com>
2020-06-22 15:29:40 +0800

    fix bug

--- Algorithm4/src/ds/Transaction.kt
+++ Algorithm4/src/ds/Transaction.kt
@@ -1,6 +1,48 @@
 package ds
 
 class Transaction : Comparable<Transaction> {
+    private val who: String
+    private val time: Date
+    private val amount: Double
+
+    constructor(
+        w: String,
+        t: Date,
+        a: Double
+    ) {
+        if (!isAmountValid(a))
+            throw IllegalArgumentException("输入金额格式错误!")
+        who = w
+        time = t
+        amount = a
+    }
+
+    constructor(
+        parseString: String
+    ) {
+        parseString.split(" ").run {
+            if (size < 3) {
+                throw IllegalArgumentException("输入格式错误！")
+            }
+            who = get(0)
+            time = Date(get(1))
+            amount = get(2).toDouble()
+            if (!isAmountValid(amount))
+                throw IllegalArgumentException("输入金额错误！")
+        }
+
+    }
+
+    private fun isAmountValid(amount: Double): Boolean {
+        if (
+            Double.NaN == amount ||
+            Double.NEGATIVE_INFINITY == amount ||
+            Double.POSITIVE_INFINITY == amount
+        ) {
+            return false
+        }
+        return true
+    }
 
 
     override fun compareTo(other: Transaction): Int {
--- Algorithm4/src/fundamentals/DateAndTransactionTest.kt
+++ Algorithm4/src/fundamentals/DateAndTransactionTest.kt
@@ -1,8 +1,10 @@
 package fundamentals
 
 import ds.Date
+import ds.Transaction
 
 fun main() {
     println(Date("2002/1/12"))
     println(Date(1, 2, 3))
+    println(Transaction("T 2002/5/22 11.99"))
 }
