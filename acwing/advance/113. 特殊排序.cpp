1efecdcc723bd85ae50e1fe8aacbd14eb1e676f3
xmmmmmovo <13256172360@163.com>
2021-01-16 14:32:25 +0800

    并查集

---
+++ acwing/advance/113. 特殊排序.cpp
@@ -0,0 +1,21 @@
+/**
+ * author: xmmmmmovo
+ * generation time: 2021/01/16
+ * filename: 113. 特殊排序.cpp
+ * language & build version : C 11 & C++ 17
+*/
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Forward declaration of compare API.
+// bool compare(int a, int b);
+// return bool means whether a is less than b.
+
+class Solution {
+public:
+    vector<int> specialSort(int N) {
+    }
+};
